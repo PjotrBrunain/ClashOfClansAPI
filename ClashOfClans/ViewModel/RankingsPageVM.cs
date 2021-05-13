f8dff1dc62c5145d7a90d8bc9e3904972179e9d1
PjotrBrunain <pjotr.brunain@gmail.com>
2021-05-13 13:33:55 +0200

    Added rankingsList

---
+++ ClashOfClans/ViewModel/RankingsPageVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClashOfClans.Model;
+using GalaSoft.MvvmLight;
+
+namespace ClashOfClans.ViewModel
+{
+    class RankingsPageVM : ViewModelBase
+    {
+        private List<UserInfo> _rankingsList = new List<UserInfo>();
+
+        public List<UserInfo> RankingsList
+        {
+            get => _rankingsList;
+            set => _rankingsList = value;
+        }
+
+        private UserInfo _selectedUserInfo;
+
+        public UserInfo SelectedUserInfo
+        {
+            get => _selectedUserInfo;
+            set => _selectedUserInfo = value;
+        }
+    }
+}
